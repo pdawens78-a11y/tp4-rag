64499f866f5936789c97c0676269b2221108e135
Dawens Pierre <pdawens78@gmail.com>
2026-05-21 20:04:07 -0400

    Test3 - Transformation de la question

---
+++ src/main/java/tp4_rag/Test3Transformation.java
@@ -0,0 +1,148 @@
+package tp4_rag;
+
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentParser;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.loader.ClassPathDocumentLoader;
+import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.rag.DefaultRetrievalAugmentor;
+import dev.langchain4j.rag.RetrievalAugmentor;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.rag.query.transformer.CompressingQueryTransformer;
+import dev.langchain4j.rag.query.transformer.QueryTransformer;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Le RAG facile !
+ */
+public class Test3Transformation {
+
+    private static void configureLogger() {
+        // Configure le logger sous-jacent (java.util.logging)
+        Logger packageLogger = Logger.getLogger("dev.langchain4j");
+        packageLogger.setLevel(Level.FINE); // Ajuster niveau
+        // Ajouter un handler pour la console pour faire afficher les logs
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setLevel(Level.FINE);
+        packageLogger.addHandler(handler);
+    }
+
+
+    // Assistant conversationnel
+    interface Assistant {
+        // Prend un message de l'utilisateur et retourne une réponse du LLM.
+        String chat(String userMessage);
+    }
+
+    public static void main(String[] args) {
+
+        configureLogger();
+
+        String llmKey = System.getenv("GEMINI_KEY");
+        if (llmKey == null || llmKey.isBlank()) {
+            System.out.println("La variable d'environnement GEMINI_KEY n'est pas définie.");
+            return;
+        }
+
+        // Mettre une température qui ne dépasse pas 0,3.
+        // Le RAG sert à mieux contrôler l'exactitude des informations données par le LLM
+        // et il est donc logique de diminuer la température.
+        ChatModel model = GoogleAiGeminiChatModel.builder()
+                .apiKey(llmKey)
+                .modelName("gemini-2.5-flash")
+                .temperature(0.3)
+                .logRequestsAndResponses(true)
+                .build();;
+
+        // -- Phase 1 du RAG : enregistrement des embeddings --
+
+        // Chargement du document ApacheTika, sous la forme d'embeddings, dans une base vectorielle en mémoire
+        DocumentParser parser = new ApacheTikaDocumentParser();
+        Document document = ClassPathDocumentLoader.loadDocument("rag.pdf", parser);
+
+        // Découpage en morceaux ou chunks
+        DocumentSplitter splitter = DocumentSplitters.recursive(300, 30);
+        List<TextSegment> segments = splitter.split(document);
+
+        // Création du modèle d'embedding
+        EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
+
+        // Calcul des embeddings
+        List<Embedding> embeddings = embeddingModel.embedAll(segments).content();
+
+        // Enregistrement dans le magasin d'embeddings
+        EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        embeddingStore.addAll(embeddings, segments);
+
+        // -- PHASE 2 : Récupération et réponse --
+
+        // Création du ContentRetriever : max 2 résultats, score >= 0.5
+        EmbeddingStoreContentRetriever contentRetriever = EmbeddingStoreContentRetriever.builder()
+                .embeddingStore(embeddingStore)
+                .embeddingModel(embeddingModel)
+                .maxResults(2)
+                .minScore(0.5)
+                .build();
+
+        // Mémoire pour 10 messages
+        MessageWindowChatMemory memory = MessageWindowChatMemory.withMaxMessages(10);
+
+        // Création du QueryTransformer
+        QueryTransformer queryTransformer = CompressingQueryTransformer.builder()
+                .chatModel(model)
+                .build();
+
+        // Création du RetrievalAugmentor
+        RetrievalAugmentor retrievalAugmentor = DefaultRetrievalAugmentor.builder()
+                .queryTransformer(queryTransformer)
+                .contentRetriever(contentRetriever)
+                .build();
+
+        // Création de l'assistant conversationnel, avec une mémoire.
+        // L'implémentation de Assistant est faite par LangChain4j.
+        // L'assistant gardera en mémoire les 10 derniers messages.
+        // La base vectorielle en mémoire est utilisée pour retrouver les embeddings.
+        Assistant assistant = AiServices.builder(Assistant.class)
+                .chatModel(model)
+                .chatMemory(memory)
+                .contentRetriever(contentRetriever)
+                .build();
+
+        // Pour plusieurs questions sans recompiler
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("==================================================");
+                System.out.println("Posez votre question : ");
+                String question = scanner.nextLine();
+                if (question.isBlank()) {
+                    continue;
+                }
+                System.out.println("==================================================");
+                if ("fin".equalsIgnoreCase(question)) {
+                    break;
+                }
+                String reponse = assistant.chat(question);
+                System.out.println("Assistant : " + reponse);
+                System.out.println("==================================================");
+            }
+        }
+    }
+
+}
+
